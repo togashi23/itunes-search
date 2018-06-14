88d59c9a1ec7c3c6c514cf7293a7f29a9dbdc39b
togashi23 <togashi10@gmail.com>
2018-06-14 23:25:33 +0900

    検索ボックスに自動でフォーカス

--- src/components/SearchTerm.vue
+++ src/components/SearchTerm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input-group mb-3">
-    <input type="text" class="form-control" v-model="term" @keydown.13="search">
+    <input type="text" class="form-control" v-model="term" @keydown.13="search" v-focus>
     <div class="input-group-append">
       <button class="btn btn-outline-primary" type="button" @click.prevent="search">検索</button>
     </div>
@@ -22,6 +22,13 @@ export default {
       },
       set(value) {
         this.$store.commit('search/setTerm', value);
+      }
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus();
       }
     }
   }
